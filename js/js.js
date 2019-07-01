1b5e54a719ecb1819a547b4b9488d83e3f99ab22
Vladislav <the_best_boy04@mail.ru>
2019-07-01 23:32:48 +0600

    New Design(demo)

--- js/js.js
+++ js/js.js
@@ -1,40 +1,24 @@
-$('a[href*="#"]').click(function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-});
-
-$('#main').on('mousemove', (e) => {
-    const x = e.pageX / $(window).width();
-    const y = e.pageY / $(window).height();
-    
-    $('#main .parallax-bg').css({
-        '-webkit-transform' : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        '-moz-transform'    : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        '-ms-transform'     : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        '-o-transform'      : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        'transform'         : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)'
-    });
-});
-
-$('#about').on('mousemove', (e) => {
-    const x = e.pageX / $(window).width();
-    const y = e.pageY / $(window).height();
-    
-    $('#about .parallax-bg').css({
-        '-webkit-transform' : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        '-moz-transform'    : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        '-ms-transform'     : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        '-o-transform'      : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)',
-        'transform'         : 'translate(-' + x*30 + 'px, -' + y*30 + 'px)'
-    });
-});
-
-document.body.onload = function() {
-    setTimeout(function() {
-        $('.loader').fadeOut()
-    }, 1000)
+var linkNav = document.querySelectorAll('[href^="#"]'),
+    V = .25;
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var w = window.pageYOffset,
+            hash = this.href.replace(/[^#]*(.*)/, '$1');
+        t = document.querySelector(hash).getBoundingClientRect().top,
+            start = null;
+        requestAnimationFrame(step);
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash
+            }
+        }
+    }, false);
 }
-
   
